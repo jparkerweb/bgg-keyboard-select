9f53cda5bd3a8085e99ee4c78dc9c86da47e8bc0
Justin Parker <jparkerweb@gmail.com>
2022-12-01 17:02:15 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,12 +1,12 @@
 
 if (window.location.href.indexOf('https://boardgamegeek.com/subscriptions') == 0) {
-	console.log("--------->list")
+	console.log("---------> 💉 injecting script for Forum Subscriptions")
 	_script = document.createElement('script');
 	_script.setAttribute('src', chrome.runtime.getURL('injectList.js'));
 	(document.head||document.documentElement).appendChild( _script  );
 	_script.parentNode.removeChild( _script);
 } else if (window.location.href.indexOf('https://boardgamegeek.com/thread') == 0) {
-	console.log("--------->thread")
+	console.log("---------> 💉 injecting script for Forum Thread")
 	_script = document.createElement('script');
 	_script.setAttribute('src', chrome.runtime.getURL('injectThread.js'));
 	(document.head||document.documentElement).appendChild( _script  );
